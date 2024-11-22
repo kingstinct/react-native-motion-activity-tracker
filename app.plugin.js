caf8245dbece3320020bcd672b2a7ef7dc0e3aa6
Ryan Iguchi <ryan.iguchi1@gmail.com>
2024-11-22 14:25:40 +0100

    adding plugin

--- app.plugin.js
+++ app.plugin.js
@@ -1,11 +1,1 @@
-const { createRunOncePlugin } = require("expo/config-plugins");
-
-const withMotionActivityPermissions =
-  require("./config-plugin/withMotionActivityPermissions").default;
-const pkg = require("./package.json");
-
-module.exports = createRunOncePlugin(
-  withMotionActivityPermissions,
-  pkg.name,
-  pkg.version,
-);
+module.exports = require("./plugin/build");
